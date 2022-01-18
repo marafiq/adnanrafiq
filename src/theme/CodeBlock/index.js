2a541177701e6423fd960e4d5025b909dee719ba
Adnan Rafiq <Adnan.Rafiq@cengage.com>
2022-01-18 11:33:24 -0500

    titlewithimagename

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -82,7 +82,7 @@ export default function CodeBlock({
                                 domtoimage.toJpeg(card, { quality: 0.95 })
                                     .then(function (dataUrl) {
                                         var link = document.createElement('a');
-                                        link.download = 'my-image-name.jpeg';
+                                        link.download = `${codeBlockTitle.replace(/ /g, "")}.jpeg`;
                                         link.href = dataUrl;
                                         link.click();
                                         setTimeout(() => cardButton.style.display='block', 2000);
