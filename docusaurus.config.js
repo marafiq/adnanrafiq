c331eadf74f11035ef390e27280d3e2a8ff78d5d
Adnan <madnan.rafiq@outlook.com>
2022-09-10 07:27:21 +0000

    no plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,100 +6,93 @@ const darkCodeTheme = require('prism-react-renderer/themes/palenight');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "Adnan Rafiq's Blog, Memories and Opinions" ,
-    tagline: 'Developer Insights',
-    url: 'https://adnanrafiq.com/',
-    baseUrl: '/',
-    onBrokenLinks: 'throw',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
-    trailingSlash: true,
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
+  title: "Adnan Rafiq's Blog, Memories and Opinions",
+  tagline: 'Developer Insights',
+  url: 'https://adnanrafiq.com/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  trailingSlash: true,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
   plugins: [
     [
-      '@docusaurus/plugin-ideal-image',
-      {
-        quality: 70,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
-        steps: 2, // the max number of images generated between min and max (inclusive)
-        disableInDev: false,
-      },
+
     ],
   ],
-    /*plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],https://github.com/cmfcmf/docusaurus-search-local*/
-    presets: [
-        [
-            'classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: false,
-                blog: {
-                    remarkPlugins: [[require('mdx-mermaid'), { mermaid: { config:{sequence: { showSequenceNumbers: true } }} }]],
-                    showReadingTime: true,
-                    editUrl: 'https://github.com/marafiq/adnanrafiq/edit/main/',
-                    feedOptions: {
-                        title: "Adnan Rafiq Blog",
-                        language: "en-US",
-                        type: "all",
-                        description: "A blog written by Adnan Rafiq - A Senior Software Engineer",
-                        copyright: `Copyright © ${new Date().getFullYear()} Adnan Rafiq`
-                    },
-                    sortPosts: "descending",
-                    blogSidebarCount:"ALL",
+  /*plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],https://github.com/cmfcmf/docusaurus-search-local*/
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          remarkPlugins: [[require('mdx-mermaid'), { mermaid: { config: { sequence: { showSequenceNumbers: true } } } }]],
+          showReadingTime: true,
+          editUrl: 'https://github.com/marafiq/adnanrafiq/edit/main/',
+          feedOptions: {
+            title: "Adnan Rafiq Blog",
+            language: "en-US",
+            type: "all",
+            description: "A blog written by Adnan Rafiq - A Senior Software Engineer",
+            copyright: `Copyright © ${new Date().getFullYear()} Adnan Rafiq`
+          },
+          sortPosts: "descending",
+          blogSidebarCount: "ALL",
 
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-                sitemap: {
-                    changefreq: "weekly",
-                    priority: 0.5
-                },
-                gtag: {
-                    trackingID: 'UA-173098616-1',
-                    anonymizeIP: true
-                }
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5
+        },
+        gtag: {
+          trackingID: 'UA-173098616-1',
+          anonymizeIP: true
+        }
 
-            }),
-        ],
+      }),
     ],
+  ],
 
-    themeConfig:
+  themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: 'Adnan Rafiq',
-                logo: {
-                    alt: 'Adnan Rafiq Logo',
-                    src: 'img/logo.png',
+    ({
+      navbar: {
+        title: 'Adnan Rafiq',
+        logo: {
+          alt: 'Adnan Rafiq Logo',
+          src: 'img/logo.png',
 
-                },
-                items: [
+        },
+        items: [
 
-                    {to: '/blog', label: 'Blog', position: 'left'},
-                    {to: '/cards', label: 'Cards', position: 'left'},
-                    {
-                        href: 'https://github.com/marafiq',
-                        label: 'GitHub',
-                        position: 'right',
-                    },
-                ],
-            },
-            footer: {
-                style: 'dark',
-                copyright: `Copyright © ${new Date().getFullYear()} Adnan Rafiq.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-                additionalLanguages: ['sql', 'csharp','powershell'],
-                defaultLanguage: 'csharp'
-            },
-        }),
+          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: '/cards', label: 'Cards', position: 'left' },
+          {
+            href: 'https://github.com/marafiq',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Adnan Rafiq.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['sql', 'csharp', 'powershell'],
+        defaultLanguage: 'csharp'
+      },
+    }),
 };
 
 module.exports = config;
